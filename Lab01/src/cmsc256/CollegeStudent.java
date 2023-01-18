93bf309a5059700037ec1ad2cb8f36d2f71694f4
NoahH99 <noah@noahhendrickson.com>
2023-01-18 18:39:13 -0500

    Finished Lab01!

--- Lab01/src/cmsc256/CollegeStudent.java
+++ Lab01/src/cmsc256/CollegeStudent.java
@@ -2,32 +2,46 @@ package cmsc256;
 
 public class CollegeStudent extends Person {
 
-    private Level level;
+    private final String[] validLevels = {"Freshman", "Sophomore", "Junior", "Senior", "Graduate"};
+
+    private String level;
 
     public CollegeStudent() {
         super();
-        level = Level.Freshman;
+        level = validLevels[0];
     }
 
-    public CollegeStudent(String first, String middle, String last, Address address, String phone, String email, Level level) {
+    public CollegeStudent(String first, String middle, String last, Address address, String phone, String email, String level) throws IllegalArgumentException  {
         super(first, middle, last, address, phone, email);
+
+        if (!validateLevel(level)) throw new IllegalArgumentException("Not a valid level!");
+
         this.level = level;
     }
 
-    public Level getLevel() {
+    private boolean validateLevel(String level) {
+        for (String validLevel : validLevels) {
+            if (validLevel.equalsIgnoreCase(level)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public String[] getValidLevels() {
+        return validLevels;
+    }
+
+    public String getLevel() {
         return level;
     }
 
-    public void setLevel(Level level) {
+    public void setLevel(String level) {
         this.level = level;
     }
 
     @Override
     public String toString() {
         return super.toString() + "College Level: " + level + "\n";
-    }
-
-    public enum Level {
-        Freshman, Sophomore, Junior, Senior, Graduate
     }
 }
